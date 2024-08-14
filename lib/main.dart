6d629ea8f50820d74e80115909e2b98e647bcbd9
RaghadKatbi <137506665+RaghadKatbi@users.noreply.github.com>
2024-08-14 18:28:55 +0300

    add my them

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:real_estate/core/mythem.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,11 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+      theme: appTheme,
       home: const MyHomePage(),
     );
   }
