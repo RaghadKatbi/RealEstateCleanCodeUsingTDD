e265e42a0d22be8f959e2098518331212e9322c3
RaghadKatbi <137506665+RaghadKatbi@users.noreply.github.com>
2024-08-22 21:58:13 +0300

    add favorite use case

---
+++ lib/feautre/user_estate/domain/usecase/set_favorite_and_unset_usecase.dart
@@ -0,0 +1,13 @@
+import 'package:dartz/dartz.dart';
+import 'package:real_estate/core/error/failer.dart';
+import 'package:real_estate/feautre/user_estate/domain/repository/estate_added_and_favorite_by_user_repository.dart';
+
+class SetFavoriteAndUnsetUseCase {
+  final EstateAddedAndFavoriteByUserRepository repository;
+
+  SetFavoriteAndUnsetUseCase({required this.repository});
+
+  Future<Either<Failure, Unit>> call(int idEstate) async {
+    return await repository.setFavoriteAndUnset(idEstate);
+  }
+}
